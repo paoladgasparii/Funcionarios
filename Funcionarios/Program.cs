391d0d59b1c3d1b049e18b8205f6c63b297e9799
Usuario <Usuario@CPD03-M03>
2024-03-26 12:30:48 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionarios/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int continuar = 1;
+            double salarioliquido = 0;
+            
+
+            while (continuar == 1)
+            {
+                Console.WriteLine("Insira a forma de contratação (A)ssalariado (C)omissionado (H)orista: ");
+                string contratacao = Console.ReadLine();
+
+                switch (contratacao)
+                {
+                    case "A":
+                        Console.WriteLine("Digite o valor do salário bruto: ");
+                        double salariob = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o valor do desconto: ");
+                        double desc = double.Parse(Console.ReadLine());
+
+                        salarioliquido = salariob - desc;
+                        break;
+
+                    case "C":
+                        Console.WriteLine("Digite a quantidade de peças produzidas: ");
+                        int pecas = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o valor recebido por peça: ");
+                        double valorpecas = double.Parse(Console.ReadLine());
+
+                        salarioliquido = pecas * valorpecas;
+                        break;
+
+                    case "H":
+                        Console.WriteLine("Digite a quantidade de horas trabalhadas: ");
+                        int hora = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite o valor recebido por hora: ");
+                        double valorhora = double.Parse(Console.ReadLine());
+
+                        salarioliquido = hora * valorhora;
+                        break;
+                }
+
+                Console.WriteLine("Deseja continuar? (1)Sim (2)Não: ");
+                continuar = int.Parse(Console.ReadLine());
+                cont++;
+                salarioliquido++;
+            }
+
+            double folhapagamento = salarioliquido + salarioliquido;
+            double mediasalarial = folhapagamento / cont;
+            Console.WriteLine("Folha de pagamento R$: " + folhapagamento);
+            Console.WriteLine("Funcionários cadastrados: " + cont);
+            Console.WriteLine("Média salarial R$: " + mediasalarial);
+
+            Console.ReadKey();
+
+        }
+    }
+}
